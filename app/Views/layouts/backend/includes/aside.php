c1709dc30a938c8d5db417c1bbf290c853becf13
Rendhi-ricr <77826101+Rendhi-ricr@users.noreply.github.com>
2024-03-04 21:38:06 +0700

    perubahan logo

--- app/Views/layouts/backend/includes/aside.php
+++ app/Views/layouts/backend/includes/aside.php
@@ -22,20 +22,20 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Interface
+        Master Data
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-solid fa-user"></i>
-            <span>Components</span>
+            <span>Master anggota</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
+
+                <a class="collapse-item" href="buttons.html">Data Anggota</a>
+
             </div>
         </div>
     </li>
