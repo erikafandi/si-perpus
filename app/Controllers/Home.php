ae1fbb8e02defcd8b34eba586ccaf5f0e8a2faf6
Rendhi-ricr <77826101+Rendhi-ricr@users.noreply.github.com>
2024-03-22 21:36:28 +0700

    TERBARUUUUU

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,23 +2,13 @@
 
 namespace App\Controllers;
 
-use App\Models\bukuModels;
+
 
 class Home extends BaseController
 {
-    protected $bukuModels;
-    public function __construct()
-    {
-        $this->bukuModels = new bukuModels();
-    }
+
     public function index()
     {
-        $buku = $this->bukuModels->orderBy("tanggal_input_buku", "DESC")->limit(4)->find();
-
-        $data = [
-            'buku' => $buku,
-        ];
-
-        return view('pages/frontend/home', $data);
+        return view('pages/frontend/home');
     }
 }
