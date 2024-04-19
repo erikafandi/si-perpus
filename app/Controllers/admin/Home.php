a367fed281185daa0e27f62b92bc033191c62bed
Rendhi-ricr <77826101+Rendhi-ricr@users.noreply.github.com>
2024-04-19 09:42:00 +0700

    Dashboard Beres

--- app/Controllers/admin/Home.php
+++ app/Controllers/admin/Home.php
@@ -3,11 +3,24 @@
 namespace App\Controllers\admin;
 
 use App\Controllers\BaseController;
+use App\Models\anggotaModels;
+use App\Models\bukuModels;
+use App\Models\transaksiModels;
 
 class Home extends BaseController
 {
     public function index()
     {
-        return view('pages/backend/home');
+        $buku = count(model(bukuModels::class)->findAll());
+        $transaksi = count(model(transaksiModels::class)->findAll());
+        $anggota = count(model(anggotaModels::class)->findAll());
+
+        $data = [
+            'buku' => $buku,
+            'transaksi' => $transaksi,
+            'anggota' => $anggota,
+        ];
+
+        return view('pages/backend/home', $data);
     }
 }
